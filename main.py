80075b7e597abe5bbbb1d08b5bbf985e32036fa5
yusragcelikel <yusragcelikel@gmail.com>
2023-10-18 19:48:11 +0300

    The oldest business on each continent

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ sorted_businesses = businesses.sort_values(by="year_founded")
 print(sorted_businesses.head())
 
 
+
 # Load countries.csv to a DataFrame
 countries = pd.read_csv("/Users/yusragokcecelikel/Downloads/CSV files/oldest_business_dataset/countries.csv")
 
@@ -20,3 +21,15 @@ businesses_countries = sorted_businesses.merge(countries, on="country_code" )
 # Filter businesses_countries to include countries in North America only
 north_america = businesses_countries[businesses_countries["continent"] == "North America"]
 print(north_america.head())
+
+
+
+# Create continent, which lists only the continent and oldest year_founded
+continent = businesses_countries[["continent", "year_founded"]].sort_values(by="year_founded").drop_duplicates(subset="continent").set_index("continent")
+
+# Merge continent with businesses_countries
+merged_continent = continent.merge(businesses_countries, on=["continent", "year_founded"])
+
+# Subset continent so that only the four columns of interest are included
+subset_merged_continent = merged_continent[["continent", "country", "business", "year_founded"]]
+print(subset_merged_continent)
