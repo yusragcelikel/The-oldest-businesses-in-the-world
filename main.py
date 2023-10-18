93e36d2003d3bd8a372800415426103ed9330e97
yusragcelikel <yusragcelikel@gmail.com>
2023-10-18 18:51:55 +0300

    The oldest businesses in North America

--- main.py
+++ main.py
@@ -11,3 +11,12 @@ sorted_businesses = businesses.sort_values(by="year_founded")
 print(sorted_businesses.head())
 
 
+# Load countries.csv to a DataFrame
+countries = pd.read_csv("/Users/yusragokcecelikel/Downloads/CSV files/oldest_business_dataset/countries.csv")
+
+# Merge sorted_businesses with countries
+businesses_countries = sorted_businesses.merge(countries, on="country_code" )
+
+# Filter businesses_countries to include countries in North America only
+north_america = businesses_countries[businesses_countries["continent"] == "North America"]
+print(north_america.head())
